281015b5423a6e307035413136715f611f3f6f2f
jovaps <fiirstcrw@hotmail.com>
2025-06-28 18:17:04 -0300

    feat: render ffpmeg with localhost

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-black">
+      <img
+        src="http://localhost:3000/camera/stream"
+        alt="Camera Stream"
+        className="w-full max-w-4xl border border-white"
+      />
+    </div>
+  );
+}
